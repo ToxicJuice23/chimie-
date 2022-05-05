2d66dd392f66f499433e61f9e50ce9ecbf2d981c
ToxicJuice23 <jujur2300@gmail.com>
2022-05-05 01:44:31 +0000

    Huge update: encoder

---
+++ encoder/encoder.cpp
@@ -0,0 +1,57 @@
+#include "encoder.h"
+
+string encoder::vector_to_string(vector<char> v) { 
+  string s(v.begin(), v.end());
+  return s;
+}
+
+int encoder::write(string filename, string _string, char method='a') {
+  if (method == 'a') {
+    fstream file; file.open(filename, ios_base::app);
+    file << _string;
+    file.close();
+  } else {
+    fstream file; file.open(filename, ios::out);
+    file << _string;
+    file.close();
+  }
+  return 0;
+}
+
+string encoder::encode(string _string) {
+  vector<char> _result;
+  for (char character : _string) {
+    for (pair<char, char> _pair : letters) {
+      if (character == _pair.first) {
+        _result.push_back(_pair.second);
+        break;
+      }
+    }
+  }
+  return this->vector_to_string(_result);
+}
+
+string encoder::translate(string encoded) {
+  vector<char> _result;
+  int x = 0;
+  for (char character : encoded) {
+    for (pair<char, char> _pair : letters) {
+      if (character == _pair.second) {
+        _result.push_back(_pair.first);
+        break;
+      }
+    }
+    x++;
+  }
+  return this->vector_to_string(_result);
+}
+
+vector<string> encoder::read(string filename) {
+  fstream file; file.open(filename, ios::in);
+  string line;
+  vector<string> lines;
+  while (getline(file, line)) {
+    lines.push_back(this->translate(line));
+  }
+  return lines;
+}
---
+++ encoder/encoder.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#define p(x,y) make_pair(x,y)
+using namespace std;
+
+class encoder {
+  private:
+    vector<pair<char, char>> letters = {
+      make_pair('a',char(1)), make_pair('b',char(3)), make_pair('c',char(2)), make_pair('d',char(5)), p('d',char(6)), p('e',char(4)), p('f',char(7)), p('g',char(9)), p('h',char(11)), p('i',char(10)), p('j',char(8)), p('k',char(13)), p('l',char(12)), p('m',char(14)), p('n',char(15)), p('o',char(16)), p('p',char(17)), p('q',char(20)), p('r',char(19)), p('s',char(18)), p('t',char(24)), p('u',char(22)), p('v',char(23)), p('w',char(21)), p('x',char(26)), p('y',char(25)), p('z',char(27)), p(' ',char(28))
+    };
+  public:
+    string encode(string _string);
+    string translate(string encoded);
+    string vector_to_string(vector<char> v);
+    int write(string filename, string _string, char method);
+    vector<string> read(string filename);
+};
